1adcd99e6e5840b3479f29eefed48f942a77340c
Leonardo Perez <83437994+lenissius12@users.noreply.github.com>
2021-05-26 16:52:40 -0500

    Banco_Registros

--- BancoReg.v
+++ BancoReg.v
@@ -1,25 +1,26 @@
 `timescale 1ns/1ns
-module BR(
-    input [4:0]RA1,
-    input [4:0]RA2,
-    input [31:0]Di,
-    input [4:0]Dir,
-    input RWrite,
-    output reg[31:0]Dr1,
-    output reg[31:0]Dr2
+module Banco_Registros(
+    input [4:0]ReadRegister1,
+    input [4:0]ReadRegister2,
+    input [31:0]WriteData,
+    input [4:0]WriteRegister,
+    input RegWrite,
+    output reg[31:0]ReadData1,
+    output reg[31:0]ReadData2
 );
 reg [31:0] BRM [0:31];
 initial begin
     $readmemb("BR.MEM",BRM);
-    $100;
+    
 end
 always @*
 begin
-    if (RWrite!=0)
+    ReadData1 = BRM[ReadRegister1];
+    ReadData2 = BRM[ReadRegister2];
+    if (RegWrite)
     begin
-        BRM[Dir] <= Di;
+        BRM[WriteRegister] <= WriteData;
     end
-    Dr1 <= BRM[RA1];
-    Dr2 <= BRM[RA2];
+
 end
 endmodule
