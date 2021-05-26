96a12d9411062eb3451d7f7cda003f47aabde89b
Leonardo Perez <83437994+lenissius12@users.noreply.github.com>
2021-05-26 16:51:00 -0500

    ALUCONTROL

--- ALUControl.v
+++ ALUControl.v
@@ -1,43 +1,52 @@
 `timescale 1ns/1ns
-module ALC(
-    input [5:0] Ins,
-    input [2:0] Code,
-    output reg[2:0] OutAlu   
+module ALUCONTROL(
+    input [5:0] Instruction,
+    input [2:0] ALUOp,
+    output reg[3:0] OutAlu   
 );
 always @* 
 begin
-    if (Code == 001)
+    if (ALUOp == 3'b001)
     begin
-      case (Ins)
+      case (Instruction)
           6'b100000:
           begin
-            OutAlu <= 3'd0;    
+            OutAlu = 4'd0;    
           end
           6'b100010:  
           begin
-            OutAlu <= 3'd1;   
+            OutAlu = 4'd1;   
           end
           6'b100100:  
           begin
-            OutAlu <= 3'd2;   
+            OutAlu = 4'd2;   
           end
           6'b100101:  
           begin
-            OutAlu <= 3'd3;   
+            OutAlu = 4'd3;   
           end
           6'b101010:  
           begin
-            OutAlu <= 3'd4;   
+            OutAlu = 4'd4;   
+          end
+	  6'b000000:  
+          begin
+            OutAlu = 4'd5;   
+          end
+	  6'b011000:  
+          begin
+            OutAlu = 4'd6;   
+          end
+	  6'b011010:  
+          begin
+            OutAlu = 4'd7;   
           end
         default
         begin
-            OutAlu <= 3'dx;
+            OutAlu = 4'dx;
         end
         endcase
     end
-    else
-    begin
-      OutAlu <= 3'dx;
-    end
+    
 end
 endmodule
