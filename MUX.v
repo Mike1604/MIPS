cc29e8692daecee572b2d7b36653f837a9708862
Paulina Guerra <84627321+guerrap02@users.noreply.github.com>
2021-06-16 20:55:51 -0500

    upload

---
+++ MUX.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ns
+
+module Multiplexor(
+	input [31:0]OP0,
+	input [31:0]OP1,
+	input Dec,
+	output reg[31:0]Salida
+);
+
+always@*
+begin
+  case(Dec)
+	1'b0:
+	begin
+	  Salida = OP0;
+	end
+
+	1'b1:
+	begin
+	  Salida = OP1;
+	end	
+  endcase
+end
+endmodule
+
---
+++ SignExtend.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ns
+
+module SignExtend(
+	input [15:0]SignInput,
+	output [31:0]Extend
+
+);
+
+assign Extend = {{16{SignInput[15]}},SignInput};
+
+endmodule
